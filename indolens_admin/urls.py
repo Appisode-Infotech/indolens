f21542aaf7a07dc8d1d80382f1a54e5fe85914f2
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2023-10-14 17:09:02 +0530

    serch and modal impl in area head

--- indolens_admin/urls.py
+++ indolens_admin/urls.py
@@ -101,7 +101,7 @@ urlpatterns = [
     path('manage_area_head/create_area_head/', createAreaHead, name='create_area_head'),
     path('manage_area_head/edit_area_head/', editAreaHead, name='edit_area_head'),
     path('manage_area_head/view_area_head/?areaHeadId=<int:ahid>', viewAreaHead, name='view_area_head'),
-    path('manage_area_head/enable_disable_area_head/?areaHeadId=<int:ahid>/?status=<int:status>', enableDisableAreaHead,
+    path('manage_area_head/enable_disable_area_head/?areaHeadId=<int:ahId>/?status=<int:status>', enableDisableAreaHead,
          name='enable_disable_area_head'),
     # marketing head
     path('manage_marketing_head/', manageMarketingHead, name='manage_marketing_head'),
