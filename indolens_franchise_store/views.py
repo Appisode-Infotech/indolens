4bba53730e21f265312f3ede5f349b5e1cc9c04e
Roop Raj <rooprajt@gmail.com>
2023-11-06 00:28:34 +0530

    franchise customer module implemented

--- indolens_franchise_store/views.py
+++ indolens_franchise_store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect, render
 
 from indolens_franchise_store.franchise_store_controller import franchise_store_auth_controller, \
-    franchise_store_customers_controller
-from indolens_franchise_store.franchise_store_model.franchise_store_req_model import franchise_store_employee_model
+    franchise_store_customers_controller, franchise_expense_controller, franchise_inventory_controller
+from indolens_franchise_store.franchise_store_model.franchise_store_req_model import franchise_store_employee_model, \
+    franchise_expense_model, franchise_create_stock_request_model
 
 
 # =================================ADMIN START======================================
@@ -110,40 +111,94 @@ def viewAllCustomersFranchise(request):
 
 def viewCustomerDetailsFranchise(request, customerId):
     response, status_code = franchise_store_customers_controller.get_customers_by_id(customerId)
-    print(response)
     return render(request, 'customers/viewCustomerDetailsFranchise.html', {"customers": response['customers']})
 
 
 # ================================= FRANCHISE STORE STOCK REQUESTS MANAGEMENT ======================================
 
 def createStockRequestFranchise(request):
-    return render(request, 'stockRequests/createStockRequestFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        if request.method == 'POST':
+            stock_obj = franchise_create_stock_request_model.franchise_create_stock_request_model_from_dict(request.POST)
+            response = franchise_inventory_controller.create_store_stock_request(stock_obj)
+            print(response)
+            products, status_code = franchise_inventory_controller.get_all_central_inventory_products()
+            return render(request, 'stockRequests/createStockRequestFranchise.html',
+                          {"product_list": products['product_list']})
+        else:
+            response, status_code = franchise_inventory_controller.get_all_central_inventory_products()
+            return render(request, 'stockRequests/createStockRequestFranchise.html',
+                          {"product_list": response['product_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 def viewAllStockRequestsFranchise(request):
-    return render(request, 'stockRequests/viewAllStockRequestsFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        response, status_code = franchise_inventory_controller.view_all_store_stock_request(
+            request.session.get('assigned_store_id'), '%')
+        return render(request, 'stockRequests/viewAllStockRequestsFranchise.html',
+                      {"stocks_request_list": response['stocks_request_list']})
+    else:
+        return redirect('franchise_store_login')
 
 
 def viewPendingStockRequestsFranchise(request):
-    return render(request, 'stockRequests/viewPendingStockRequestsFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        response, status_code = franchise_inventory_controller.view_all_store_stock_request(
+            request.session.get('assigned_store_id'), '0')
+        return render(request, 'stockRequests/viewPendingStockRequestsFranchise.html',
+                      {"stocks_request_list": response['stocks_request_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 def viewCompletedStockRequestsFranchise(request):
-    return render(request, 'stockRequests/viewCompletedStockRequestsFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        response, status_code = franchise_inventory_controller.view_all_store_stock_request(
+            request.session.get('assigned_store_id'), '1')
+        return render(request, 'stockRequests/viewCompletedStockRequestsFranchise.html',
+                      {"stocks_request_list": response['stocks_request_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 def viewRejectedStockRequestsFranchise(request):
-    return render(request, 'stockRequests/viewRejectedStockRequestsFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        response, status_code = franchise_inventory_controller.view_all_store_stock_request(
+            request.session.get('assigned_store_id'), '2')
+        return render(request, 'stockRequests/viewRejectedStockRequestsFranchise.html',
+                      {"stocks_request_list": response['stocks_request_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 # ================================= FRANCHISE STORE INVENTORY MANAGEMENT ======================================
 
 def franchiseInventoryProducts(request):
-    return render(request, 'inventory/franchiseInventoryProducts.html')
+
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        response, status_code = franchise_inventory_controller.get_all_products_for_franchise_store(
+            request.session.get('assigned_store_id'))
+        return render(request, 'inventory/franchiseInventoryProducts.html', {"stocks_list": response['stocks_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 def inventoryOutOfStockFranchise(request):
-    return render(request, 'inventory/inventoryOutOfStockFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        response, status_code = franchise_inventory_controller.get_all_out_of_stock_products_for_franchise_store(15,
+                                                                                                   request.session.get(
+                                                                                                       'assigned_store_id'))
+        return render(request, 'inventory/inventoryOutOfStockFranchise.html', {"stocks_list": response['stocks_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 def moveStocksFranchise(request):
@@ -153,7 +208,19 @@ def moveStocksFranchise(request):
 # ================================= SALES AND EXPENSES ======================================
 
 def allExpenseFranchise(request):
-    return render(request, 'expenses/allExpenseFranchise.html')
+    if request.session.get('is_franchise_store_logged_in') is not None and request.session.get('is_franchise_store_logged_in') is True:
+        if request.method == 'POST':
+            expense_obj = franchise_expense_model.franchise_expense_model_from_dict(request.POST)
+            response, status_code = franchise_expense_controller.create_franchise_store_expense(expense_obj)
+            return redirect('all_expenses_franchise_store')
+        else:
+            response, status_code = franchise_expense_controller.get_all_franchise_store_expense(request.session.get('assigned_store_id'),
+                                                                             request.session.get('store_type'))
+            return render(request, 'expenses/allExpenseFranchise.html',
+                          {"stor_expense_list": response['stor_expense_list']})
+    else:
+        return redirect('franchise_store_login')
+
 
 
 def makeSaleFranchiseStore(request):
