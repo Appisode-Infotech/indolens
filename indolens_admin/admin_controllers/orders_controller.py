380c275eac84c8ac19320c43ce04bd1868d24bf1
Roop Raj <rooprajt@gmail.com>
2024-01-13 13:33:39 +0530

    enhancement

--- indolens_admin/admin_controllers/orders_controller.py
+++ indolens_admin/admin_controllers/orders_controller.py
@@ -71,7 +71,8 @@ def get_all_orders(status, pay_status, store):
 
             return {
                 "status": True,
-                "orders_list": get_sales_orders(orders_list)
+                "orders_list": get_sales_orders(orders_list),
+                "latest_orders": get_sales_orders(orders_list)[:15]
             }, 200
 
     except pymysql.Error as e:
