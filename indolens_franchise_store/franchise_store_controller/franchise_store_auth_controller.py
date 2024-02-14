478c8ca2a7a92f83d2f48601d0ce3faf7fd2a8e7
Roop Raj <rooprajt@gmail.com>
2024-02-14 18:08:36 +0530

    Inactive store login restricted, accountant address update fixed

--- indolens_franchise_store/franchise_store_controller/franchise_store_auth_controller.py
+++ indolens_franchise_store/franchise_store_controller/franchise_store_auth_controller.py
@@ -54,11 +54,24 @@ def login(store_ob):
                     "store": None
                 }, 301
             elif bcrypt.checkpw(store_ob.password.encode('utf-8'), admin_data[0][4].encode('utf-8')):
-                return {
-                    "status": True,
-                    "message": "user login successfull",
-                    "store": get_franchise_store_employees(admin_data)
-                }, 200
+                store = get_franchise_store_employees(admin_data)
+                get_assigned_store_status = f"""SELECT status FROM franchise_store
+                                                WHERE store_id = '{store[0]['assigned_store_id']}'"""
+                cursor.execute(get_assigned_store_status)
+                assigned_store_status = cursor.fetchone()
+
+                if assigned_store_status[0] == 0:
+                    return {
+                        "status": False,
+                        "message": "You Account is assigned to inactive store, please contact you Admin",
+                        "store": store
+                    }, 200
+                else:
+                    return {
+                        "status": True,
+                        "message": "user login successfull",
+                        "store": store
+                    }, 200
             else:
                 return {
                     "status": False,
@@ -79,7 +92,19 @@ def get_assigned_store(employee_id):
                                 WHERE employee_id = '{employee_id}'"""
             cursor.execute(get_assigned_store)
             assigned_store = cursor.fetchone()
-            return assigned_store[0]
+
+            if assigned_store[0] != 0:
+                get_assigned_store_status = f"""SELECT status FROM franchise_store 
+                                                WHERE store_id = '{assigned_store[0]}'"""
+                cursor.execute(get_assigned_store_status)
+                assigned_store_status = cursor.fetchone()
+                if assigned_store_status[0] == 0:
+                    return 0
+                else:
+                    return assigned_store[0]
+
+            else:
+                return assigned_store[0]
     except pymysql.Error as e:
         return 0
     except Exception as e:
