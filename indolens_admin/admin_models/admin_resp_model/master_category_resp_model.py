1d930fafe7195ca7cf5f31b9a3dd051fbcdfcf5d
Roop Raj <rooprajt@gmail.com>
2024-02-20 19:44:09 +0530

    date format chnage

--- indolens_admin/admin_models/admin_resp_model/master_category_resp_model.py
+++ indolens_admin/admin_models/admin_resp_model/master_category_resp_model.py
@@ -15,9 +15,9 @@ class ProductCategory:
             'category_prefix': self.category_prefix,
             'category_description': self.category_description,
             'status': self.status,
-            'created_on': self.created_on.strftime('%d-%m-%Y %I:%M %p') if isinstance(self.created_on, datetime) else None,
+            'created_on': self.created_on.strftime('%d/%m/%Y %I:%M %p') if isinstance(self.created_on, datetime) else None,
             'created_by': self.created_by,
-            'last_updated_on': self.last_updated_on.strftime('%d-%m-%Y %I:%M %p') if isinstance(self.last_updated_on, datetime) else None,
+            'last_updated_on': self.last_updated_on.strftime('%d/%m/%Y %I:%M %p') if isinstance(self.last_updated_on, datetime) else None,
             'last_updated_by': self.last_updated_by,
             'creator_name': self.creator_name,
             'updater_name': self.updater_name
--- indolens_admin/admin_models/admin_resp_model/master_material_resp_model.py
+++ indolens_admin/admin_models/admin_resp_model/master_material_resp_model.py
@@ -14,9 +14,9 @@ class ProductMaterial:
             'material_name': self.material_name,
             'material_description': self.material_description,
             'status': self.status,
-            'created_on': self.created_on.strftime('%d-%m-%Y %I:%M %p') if isinstance(self.created_on, datetime) else None,
+            'created_on': self.created_on.strftime('%d/%m/%Y %I:%M %p') if isinstance(self.created_on, datetime) else None,
             'created_by': self.created_by,
-            'last_updated_on': self.last_updated_on.strftime('%d-%m-%Y %I:%M %p') if isinstance(self.last_updated_on, datetime) else None,
+            'last_updated_on': self.last_updated_on.strftime('%d/%m/%Y %I:%M %p') if isinstance(self.last_updated_on, datetime) else None,
             'last_updated_by': self.last_updated_by,
             'creator_name': self.creator_name,
             'updater_name': self.updater_name
