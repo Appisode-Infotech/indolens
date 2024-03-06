04a970150d63a7601356c49fb66d34f915ac5172
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2024-03-06 15:03:18 +0530

    lab modal bug fix

--- indolens_own_store/views.py
+++ indolens_own_store/views.py
@@ -328,6 +328,7 @@ def createStockRequestStore(request):
         else:
             response, status_code = store_inventory_controller.get_all_central_inventory_products(
                 assigned_store)
+            print(response)
             return render(request, 'stockRequests/createStockRequestStore.html',
                           {"product_list": response['product_list']})
     else:
