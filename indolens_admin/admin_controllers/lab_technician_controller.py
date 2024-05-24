9737be7e4370c12ffedeaaaa42981b74afd20976
Roop Raj <rooprajt@gmail.com>
2024-05-24 12:41:13 +0530

    lab sorting fix v1

--- indolens_admin/admin_controllers/lab_technician_controller.py
+++ indolens_admin/admin_controllers/lab_technician_controller.py
@@ -171,9 +171,9 @@ def assign_lab(labtechId, labId):
             update_lab_technician_query = f"""
                 UPDATE lab_technician
                 SET
-                    assigned_lab_id = {labId}
+                    lt_assigned_lab_id = {labId}
                 WHERE
-                    lab_technician_id = {labtechId}
+                    lt_lab_technician_id = {labtechId}
             """
             # Execute the update query using your cursor
             cursor.execute(update_lab_technician_query)
