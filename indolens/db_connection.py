ac78a185e6526e6888aff77c3e252e5e2a661cf9
Roop Raj <rooprajt@gmail.com>
2024-06-13 20:41:02 +0530

    Bug fix 13 June 2024 v2

--- indolens/db_connection.py
+++ indolens/db_connection.py
@@ -7,7 +7,7 @@ def getConnection():
         port=3306,
         user="root",
         password="",
-        db="2_indolens_db",
+        db="indolens_db",
         autocommit=True,
         max_allowed_packet=67108864,
         cursorclass=pymysql.cursors.DictCursor
